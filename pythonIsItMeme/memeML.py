42e402bfea783e4dbaa1fabc1d2806117b996d27
Aaron Turner <aaron@aaronthedev.com>
2016-04-18 17:26:42 -0700

    Commented code for better understanding

--- pythonIsItMeme/memeML.py
+++ pythonIsItMeme/memeML.py
@@ -1,6 +1,13 @@
 import scikit import tree
+
+#Features are weight and bumbpiness
 features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+# Corresponding labels (apple or orange) for the weights
 labels = [0, 0, 1, 1]
+
+#Using a decision tree classifier
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
+
+#Print the result!
 print clf.predict([[150, 0]])
