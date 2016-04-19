165ef0ae767747d9e77416a4566be679e83bb319
Aaron Turner <aaron@aaronthedev.com>
2016-04-18 17:57:56 -0700

    Created some simple testing data

--- pythonIsItMeme/memeML.py
+++ pythonIsItMeme/memeML.py
@@ -1,9 +1,9 @@
 import scikit import tree
 
 #Features are weight and bumbpiness
-features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+#features = [[140, 1], [130, 1], [150, 0], [170, 0]]
 # Corresponding labels (apple or orange) for the weights
-labels = [0, 0, 1, 1]
+#labels = [0, 0, 1, 1]
 
 #Using a decision tree classifier
 clf = tree.DecisionTreeClassifier()
