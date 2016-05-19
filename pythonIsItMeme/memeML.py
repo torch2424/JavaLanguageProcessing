751ad9afbbe29722f42b4b96d911154cb1580119
Aaron Turner <aaron@aaronthedev.com>
2016-05-19 01:40:57 +0000

    Finished up some basic increasing meme accuracy

--- pythonIsItMeme/memeML.py
+++ pythonIsItMeme/memeML.py
@@ -34,14 +34,51 @@ with open('trainingDataNoMeme', 'r') as f:
 #Add the nomemes to the features
 noMemeFeatures.extend(noMemeTrainList)
 
-#Create a mixed features array
-classifierFeatures = [];
-classifierFeatures.extend(memeFeatures)
-classifierFeatures.extend(noMemeFeatures)
+#Train a classifier iteratively, and use the most accurate
+maxSize = 0;
+if len(memeFeatures) > len(noMemeFeatures)
+    maxSize = len(noMemeFeatures)
+else
+    maxSize = len(memeFeatures)
 
+#Make sure maxsize is greater than 10
+if maxSize > 10
+    print "plz add some moar memes, need moar than 10 ;)"
 
-#Using a naive bayes Classifier
-bClassifier = NaiveBayesClassifier(classifierFeatures)
+#Find our index for 10 iterations
+featureIteration = len(maxSize) / 10
+
+#Create our features array, and classifiers
+classifierFeatures = []
+trainedClassifiers = []
+
+for i in range(0, featureIteration)
+
+    #Extend the needed features
+    classifierFeatures.extend(memeFeatures[(i * featureIteration): ((i + 1) * featureIteration)])
+    classifierFeatures.extend(noMemeFeatures[(i * featureIteration): ((i + 1) * featureIteration)])
+
+    #Train a classifier to be added to our array
+    trainedClassifiers.extend(NaiveBayesClassifier(classifierFeatures))
+
+#Next find the most accurate classifier
+#Find our accuracy
+testingAccuracy = [];
+with open('testingData', 'r') as f:
+    for line in f:
+        line = line.strip().lower()
+        testingAccuracy.append([line.split('%')[0], line.split('%')[1]])
+
+#Set the classifier to the first classifier
+bClassifier = trainedClassifiers[0]
+
+for i in range(1, len(trainedClassifiers))
+    if trainedClassifiers[i].accuracy(testingAccuracy) < bClassifier.accuracy(testingAccuracy)
+        bClassifier = trainedClassifiers[i]
+
+#Times 100 for percent, print our accuracy
+print("Meme Accuracy: " + str(bClassifier.accuracy(testingAccuracy) * 100) + "%")
+
 
 #Read in our testing
 testingList = [];
@@ -53,16 +90,6 @@ with open('testingData', 'r') as f:
 #Print our testing data
 for i in range(0, len(testingList)):
     print testingList[i] + " - this haz " + bClassifier.classify(testingList[i])
-
-#Find our accuracy
-testingAccuracy = [];
-with open('testingData', 'r') as f:
-    for line in f:
-        line = line.strip().lower()
-        testingAccuracy.append([line.split('%')[0], line.split('%')[1]])
-
-#Times 100 for percent
-print("Meme Accuracy: " + str(bClassifier.accuracy(testingAccuracy) * 100) + "%")
 
 #Show our most informative features
 bClassifier.show_informative_features(15)
